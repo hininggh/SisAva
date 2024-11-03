368c2e06da974d023eada1072297b915d2a7f3b8
gusta <gustavohining@gmail.com>
2024-11-03 19:55:06 -0300

    ajustado redirecionamento.

--- indicadores/views.py
+++ indicadores/views.py
@@ -8,6 +8,7 @@ from cursos.models import Curso
 from logs.views import registrar_acao_log  # Função para registrar logs
 from .forms import NivelSupostoForm, NSAForm, RelatorioForm
 import os  # Biblioteca para manipular arquivos temporários
+import tempfile
 from usuarios.models import Usuario  # Modelo de Usuario
 
 
@@ -29,6 +30,10 @@ def visualizar_indicador(request, curso_id, indicador_id):
         nivel_suposto_form = NivelSupostoForm(request.POST or None, instance=indicador_man)
         relatorio_form = RelatorioForm(request.POST or None, request.FILES or None, instance=indicador_man)
 
+    # Verificar se existe relatório e se nível suposto é None
+    has_relatorio = indicador_man.conteudo is not None
+    nivel_suposto_vazio = indicador_man.nivel_suposto is None
+
     context = {
         'curso': curso,
         'indicador_man': indicador_man,
@@ -37,6 +42,8 @@ def visualizar_indicador(request, curso_id, indicador_id):
         'relatorio_form': relatorio_form,
         'tabela_conceitos': indicador_man.indicador_info.tabela_conceitos,
         'mensagem_aviso': indicador_man.indicador_info.mensagem_aviso,
+        'has_relatorio': has_relatorio,
+        'nivel_suposto_vazio': nivel_suposto_vazio,
     }
 
     return render(request, 'indicadores/detalhesindicadorrelator.html', context)
@@ -46,6 +53,7 @@ def visualizar_indicador(request, curso_id, indicador_id):
 
 
 # Baixar Relatório
+
 @login_required
 def baixar_relatorio(request, curso_id, indicador_id):
     curso = get_object_or_404(Curso, id=curso_id)
@@ -58,14 +66,13 @@ def baixar_relatorio(request, curso_id, indicador_id):
             capa_path = curso.capa.path
 
             # Mesclar a capa e o relatório
-            merger = PdfMerger()
-            merger.append(capa_path)  # Adicionar a capa primeiro
-            merger.append(relatorio_path)  # Adicionar o relatório depois
-
-            # Criar um arquivo temporário mesclado
-            mesclado_path = f"/tmp/relatorio_mesclado_{curso.nome}_{indicador.indicador_info.nome}.pdf"
-            with open(mesclado_path, 'wb') as f_out:
-                merger.write(f_out)
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+                mesclado_path = temp_file.name
+                merger = PdfMerger()
+                merger.append(capa_path)  # Adicionar a capa primeiro
+                merger.append(relatorio_path)  # Adicionar o relatório depois
+                merger.write(temp_file)
+                merger.close()  # Fechar o merger para liberar recursos
 
             # Registrar no log
             acao = "Relatório Baixado com Capa"
@@ -74,10 +81,9 @@ def baixar_relatorio(request, curso_id, indicador_id):
             # Enviar o arquivo mesclado para o download
             with open(mesclado_path, 'rb') as f:
                 response = HttpResponse(f.read(), content_type='application/pdf')
-                response[
-                    'Content-Disposition'] = f'attachment; filename="relatorio_mesclado_{indicador.indicador_info.nome}.pdf"'
+                response['Content-Disposition'] = f'attachment; filename="relatorio_mesclado_{indicador.indicador_info.nome}.pdf"'
 
-            # Remover o arquivo temporário
+            # Remover o arquivo temporário após o envio
             os.remove(mesclado_path)
             return response
 
@@ -99,20 +105,23 @@ def baixar_relatorio(request, curso_id, indicador_id):
 @login_required
 def deletar_relatorio(request, curso_id, indicador_id):
     curso = get_object_or_404(Curso, id=curso_id)
-    indicador = get_object_or_404(IndicadorMan, id=indicador_id)
+    indicador = get_object_or_404(IndicadorMan, curso=curso, id=indicador_id)
 
     # Verificar o parâmetro de origem
     origem = request.GET.get('origem', 'indicador')  # padrão para 'indicador' se o parâmetro não for passado
 
-    if indicador.conteudo:
-        indicador.conteudo.delete()
-    indicador.data_envio = None
-    indicador.usuario_relatorio = None
-    indicador.save()
+    if request.method == 'POST' and indicador.conteudo:
+        try:
+            # Registrar a exclusão no log antes da exclusão do arquivo
+            acao = "Relatório Excluído"
+            registrar_acao_log(request.user, curso, acao, indicador)
 
-    # Registrar no log
-    acao = "Relatório Deletado"
-    registrar_acao_log(request.user, curso, acao, indicador)
+            # Apaga o arquivo fisicamente e remove a referência ao conteúdo
+            indicador.conteudo.delete(save=False)
+            indicador.conteudo = None
+            indicador.save(update_fields=['conteudo'])
+        except PermissionError:
+            os.remove(indicador.conteudo.path)  # Força a exclusão se houver erro de permissão
 
     # Redireciona para a página de origem
     if origem == 'curso':
