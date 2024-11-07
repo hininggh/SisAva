abff78350d0a91c24667c9d0d0babbe8cf00f8d6
gusta <gustavohining@gmail.com>
2024-11-07 14:12:10 -0300

    arrumado os logs

--- logs/urls.py
+++ logs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:curso_id>/logs/nsa/', views.filtrar_por_nsa, name='filtrar_por_nsa'),
     path('<int:curso_id>/logs/conceito/', views.filtrar_por_conceito, name='filtrar_por_conceito'),
     path('<int:curso_id>/logs/relatorio/', views.filtrar_por_relatorio, name='filtrar_por_relatorio'),
+    path('gerenciar-logs/', views.gerenciar_logs, name='gerenciar_logs'),
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from cursos.models import Curso
+from .models import Log
+from django.db.models import Q
 
 # Usar strings para referenciar os modelos
-from .models import Log
+
 
 # Função genérica para registrar ações no log
 def registrar_acao_log(usuario, curso, acao, indicador=None):
@@ -23,6 +26,31 @@ def registrar_acao_log(usuario, curso, acao, indicador=None):
 
 # Visualizar os logs relacionados a um curso
 @login_required
+def gerenciar_logs(request):
+    # Obter os cursos que o usuário é criador ou está na lista de relatores com privilégios
+    cursos = Curso.objects.filter(
+        Q(criador=request.user) | Q(relatores=request.user, privilegios=True)
+    ).distinct()
+
+    # Aplicar filtragem com base em parâmetros GET, se fornecidos
+    filtro_nsa = request.GET.get('filtro_nsa')
+    filtro_conceito = request.GET.get('filtro_conceito')
+    filtro_relatorio = request.GET.get('filtro_relatorio')
+
+    logs = Log.objects.all()
+
+    if filtro_nsa:
+        logs = logs.filter(tipo='NSA')
+    elif filtro_conceito:
+        logs = logs.filter(tipo='Conceito')
+    elif filtro_relatorio:
+        logs = logs.filter(tipo='Relatório')
+
+    return render(request, 'logs/gerenciarlogs.html', {
+        'cursos': cursos,
+        'logs': logs,
+    })
+
 def visualizar_logs(request, curso_id):
     curso = get_object_or_404('cursos.Curso', id=curso_id)  # Referência ao modelo Curso como string
     logs = Log.objects.filter(curso=curso).order_by('-data_hora')  # Ordena pelos mais recentes
